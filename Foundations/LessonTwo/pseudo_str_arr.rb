2aa98f29fcbed2de56e530b369213f33ae25e3f1
Zachary Williams <zlw5009@gmail.com>
2016-05-19 01:45:10 -0400

    Added Array/String concatenation pseudocode file

---
+++ Foundations/LessonTwo/pseudo_str_arr.rb
@@ -0,0 +1,12 @@
+# set arr = several sentences
+# define method to concatenate strings together
+# set string variable = the concatenated array
+# join all strings in the array into one string
+# end the method
+
+def concatenate_array
+	arr = ["this is my string", "where I concatenate", "everything together"]
+	string_arr = arr.join(" ")
+end
+
+puts concatenate_array

782fda73c1c2512f983dead042c1a0adb35ac8bf
Zachary Williams <zlw5009@gmail.com>
2016-05-19 01:30:46 -0400

    Added pseudocode file for sum of two integers

---
+++ Foundations/LessonTwo/pseudocode_sum.rb
@@ -0,0 +1,10 @@
+# declare method name with two perimeters 
+# add the two perimeters together
+# end the method
+# call the method with two perimeters
+
+def sum_ints(x,y)
+	x + y
+end
+
+puts sum_ints(5,5)

444deb4f9dd95fe0c16d37337714474c303734a8
Zachary Williams <zlw5009@gmail.com>
2016-05-20 03:27:08 -0400

    RPS Bonus features: Counter is not working properly.

--- Foundations/LessonTwo/rpsls_game.rb
+++ Foundations/LessonTwo/rpsls_game.rb
@@ -1,45 +1,100 @@
 # rock_paper_scissors_lizard_spock game
 
+# to accept the first letter of each choice 
+# could store the letter and word in a hash
+# could use a case statement that signifies what each letter stands for
+# could use a nested array
+
 VALID_CHOICES = %w(rock paper scissors lizard spock)
 
 def prompt(msg)
   puts("=> #{msg}")
 end
 
-def display_results(player, computer)
-  if (player == 'rock' && (computer == 'scissors' || computer == 'lizard')) || 
-     (player == 'scissors' && (computer == 'paper' || computer == 'lizard')) ||
-     (player == 'paper' && (computer == 'rock' || computer == 'spock')) ||
-     (player == 'lizard' && (computer == 'spock' || computer == 'paper')) ||
-     (player == 'spock' && (computer == 'scissors' || computer == 'rock'))
-    prompt("WINNER!")
-  elsif player == computer
-    prompt("It's a tie")
-  else
-    prompt("Loser :(")
+def find_abbreviation(array)
+  valid_abbreviation = []
+  array.each do |word|
+    valid_abbreviation << word.chars.first
+  end
+  valid_abbreviation
+end
+
+def word_abbreviation(letter)
+  case letter
+  when 'r'
+    'rock'
+  when 'p'
+    'paper'
+  when 'l'
+    'lizard'
+  when 'sp'
+    'spock'
+  when 'sc'
+    'scissors'
   end
 end
-  
+
+def win?(first, second)
+  (first == 'rock' && ((second == 'scissors') || (second == 'lizard'))) || 
+  (first == 'scissors' && ((second == 'paper') || (second == 'lizard'))) ||
+  (first == 'paper' && ((second == 'rock') || (second == 'spock'))) ||
+  (first == 'lizard' && ((second == 'spock') || (second == 'paper'))) ||
+  (first == 'spock' && ((second == 'scissors') || (second == 'rock')))
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("WINNER!!")
+  elsif win?(computer, player)
+    prompt("You lost!")
+  else
+    prompt("It's a tie!")
+  end
+end
 
 loop do
+  user_score = 0
+  computer_score = 0
 
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
+    choice = ''
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      choice = gets.chomp
 
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That was an incorrect choice. Please try again.")
+      if VALID_CHOICES.include?(choice) || find_abbreviation(VALID_CHOICES).include?(choice)
+        break
+      else
+        prompt("That was an incorrect choice. Please try again.")
+      end
     end
-  end
 
-  computer_choice = VALID_CHOICES.sample
+    if (choice == 's' && choice.length == 1)
+      prompt("Are you selecting scissors: Yes or No?")
+      interpretation = gets.chomp
+      if interpretation.downcase.start_with?('y')
+        choice = word_abbreviation('sc')
+      else 
+        choice = word_abbreviation('sp')
+      end
+    elsif choice.length == 1
+      choice = word_abbreviation(choice)
+    end
+      
+    computer_choice = VALID_CHOICES.sample
+    prompt("You chose #{choice}, the computer chose #{computer_choice}.")
 
-  prompt("You chose #{choice}, the computer chose #{computer_choice}.")
-
-  display_results(choice, computer_choice)
+    winner = display_results(choice, computer_choice)
+    prompt(who_won)
+  
+    if winner == 'WINNER!!' 
+      user_score = user_score + 1
+    elsif winner == 'You lost!' 
+      computer_score = computer_score + 1
+    end
+  
+    prompt("Your score is: #{user_score}. The computer's score is: #{computer_score}.")
+  
+    break if user_score > 4 || computer_score > 4
 
   prompt("Do you want to play again: Yes of No?")
   answer = gets.chomp

b5b53296c29e47476e7aaa3c5bd28020d4d60138
Zachary Williams <zlw5009@gmail.com>
2016-05-19 22:47:55 -0400

    Upgraded mortgage_calculator with verification

--- Foundations/LessonTwo/mortgage_calculator.rb
+++ Foundations/LessonTwo/mortgage_calculator.rb
@@ -12,24 +12,79 @@
 # add interest and principle together
 # print the monthly payment
 
-puts "What is the loan amount"
-loan_amt = gets.chomp
+# turn the string APR into a float and divide by 100 to get in percentage format
+# then divide by 12 to calculate the monthly IR from the APR
 
-puts "What is the APR on the loan"
-apr = gets.chomp
-
-puts "what is the duration of the loan in months"
-duration = gets.chomp
-
-# turn the string interest rate into a float and divide by 100
-
-apr = apr.to_f / 100
-
-monthly_payment = (loan_amt.to_f * (apr * (1+apr) ** duration.to_i))
-monthly_payment = monthly_payment / ((1+apr) ** duration.to_i - 1)
-puts monthly_payment.round(2)
 # utilize the following formula for the calculation of the monthly payment
 # P = L[i(1+i)^n] / [(1+i)^n - 1]
 # P = monthly payment; L = loan amount; i = interest rate
 # ** is used for exponential calculations
 
+def prompt(msg)
+	puts ("=> #{msg}")
+end
+
+def two_decimal(num)
+	num = sprintf "%.2f", num
+end
+
+prompt("Let's calculate your monthly loan payment!")
+
+loop do
+
+	prompt("What is the loan amount?")
+
+	loan_amt = ''
+	loop do
+		loan_amt = gets.chomp
+
+		if loan_amt.empty? || loan_amt.to_f < 0
+			prompt("Must enter positive loan amount.")
+		else 
+			break
+		end
+	end
+
+
+	prompt("What is the Annual Percentage Rate (APR)?")
+
+	apr = ''
+	loop do
+		apr = gets.chomp
+
+		if apr.empty? || apr.to_f < 0
+			prompt("Must enter a positive interest rate.")
+		else
+			break
+		end
+	end
+
+
+	prompt("What is the duration of the loan in months")
+
+	duration = ''
+	loop do
+		duration = gets.chomp
+
+		if duration.empty? || duration.to_i < 0
+			prompt("Enter a valid term in months")
+		else
+			break
+		end
+	end
+
+	int_rate = (apr.to_f / 100) / 12
+
+	monthly_payment = ((loan_amt.to_f * (int_rate* (1 + int_rate) ** duration.to_i)) /
+										((1 + int_rate) ** duration.to_i - 1))
+
+	prompt("The monthly payment on this loan for #{two_decimal(loan_amt)} at an APR 
+				of #{apr}% over #{duration} months is #{two_decimal(monthly_payment)}!")
+
+	prompt("Would you like to calculate another loan: Yes or No?")
+	answer = gets.chomp
+
+	break unless answer.downcase.start_with?('y')
+end
+
+prompt("Goodbye!")

0f7c2b5c98d0dbd5b55a21e23eac9789d8b7855a
Zachary Williams <zlw5009@gmail.com>
2016-05-21 03:11:07 -0400

    Wrote a string substitution program

---
+++ Foundations/lessonthree/string_substitution.rb
@@ -0,0 +1,10 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" }
+}
+
+munsters.each do |name, details|
+  puts "#{name} is a #{details['age']} year old #{details['gender']}"
+end

efe0fec05535830068629be1fff5de2f161f0660
Zachary Williams <zlw5009@gmail.com>
2016-05-22 21:21:48 -0400

    Wrote tictactoe game

---
+++ Foundations/LessonFour/tictactoe.rb
@@ -0,0 +1,121 @@
+# Tic Tac Toe
+
+# 1. Display the initial empty 3x3 board.
+# 2. Ask the user to mark a square.
+# 3. Computer marks a square.
+# 4. Display the updated board state.
+# 5. If winner, display the winner.
+# 6. If board is full, display tie. 
+# 7. If neither winner nor board is full, go to #2 (LOOP)
+# 8. Play again?
+# 9. If yes, go to #1 (LOOP)
+# 10. Goodbye!
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def display_board(brd)
+  system 'clear'
+  prompt "You're a #{PLAYER_MARKER} and the Computer is an #{COMPUTER_MARKER}"
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+end
+
+def initialize_board # Creates the board hash where we can enter a our X's and O's
+  new_board = {}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER }
+  new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select{|num| brd[num] == INITIAL_MARKER}
+end
+
+def player_turn!(brd) # Places an X for the player in the appropriate square
+  square = ''
+
+  loop do 
+    prompt("Choose a square (#{empty_squares(brd).join(', ')}):")
+    square = gets.chomp.to_i
+
+    break if empty_squares(brd).include?(square)
+      prompt("Sorry, that's not a valid choice")
+  end
+
+  brd[square] = PLAYER_MARKER
+end
+
+def computer_turn!(brd)
+  square = empty_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                  [[1, 5, 9], [3, 5, 7]]              # diagonals
+
+  winning_lines.each do |line|
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+       return 'Player'
+     elsif brd[line[0]] == COMPUTER_MARKER &&
+      brd[line[1]] == COMPUTER_MARKER &&
+      brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+loop do
+  board = initialize_board
+
+  loop do 
+    display_board(board)
+
+    player_turn!(board)
+    break if board_full?(board) || someone_won?(board)
+
+    computer_turn!(board)
+    break if board_full?(board) || someone_won?(board)
+  end 
+
+  display_board(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else 
+    prompt "It's a tie!"
+  end
+
+  prompt "Would you like to play again: Yes or No?"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thanks for playing Tic-Tac-Toe. Goodbye!"

67e8ee2f958fe37249d30846affa744e27addadc
Zachary Williams <zlw5009@gmail.com>
2016-06-12 02:20:00 -0400

    Which are objects?

---
+++ OOP120/Lesson_Four/Easy1/Q1.rb
@@ -0,0 +1,10 @@
+# Which of the following are objects in Ruby? 
+# If they are objects, how can you find out what class 
+# they belong to?
+
+true # => this is a boolean object
+"hello" # => this is a string object
+[1, 2, 3, "happy days"] # => this is an array object
+142 # => this is a fixnum object
+
+# => we can find which class they belong to by calling (some_object).class

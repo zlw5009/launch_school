2fccf35cf7c4ee8c2bc773dc922c832bb5ec82f9
Zachary Williams <zlw5009@gmail.com>
2016-06-18 01:25:05 -0400

    Wrote methods to deal cards and display them.

--- OOP120/twentyone.rb
+++ OOP120/twentyone.rb
@@ -1,66 +1,143 @@
-class Player
+require 'pry'
 
-  attr_accessor :name, :hand
+module Actionable
+  def hit!
 
-  def initialize
-    @name = ''
-    @hand = []
-  end
-
-  def hit
   end
 
   def stay
+
   end
 
   def busted?
+
   end
 
-  def total
+  def collect_card(new_card)
+    hand << new_card
   end
-end
 
-class Dealer
-  def initialize
-    @name = 'Natalia'
-    @hand = []
+  def show_hand
+    puts "Hand"
+    hand.each do |card|
+      puts "#{card}"
+    end
   end
 end
 
 class Participant
-  def initialize
+  include Actionable
 
+  attr_accessor :name, :hand
+
+  def initialize
+    @hand = []
+  end
+end
+
+class Player < Participant
+  def show_flop
+    show_hand
+  end
+end
+
+class Dealer < Participant
+  def show_flop
+    show_hand
   end
 end
 
 class Deck
-  SUITS = %w(H D C S)
-
   attr_accessor :deck
 
   def initialize
-    deck = [*2..10] + %w(J Q K A)
-    deck.product(SUITS)
+    @deck = []
+    Card::SUITS.each do |suit|
+      Card::FACES.each do |face|
+        @deck << Card.new(suit, face)
+      end
+    end
+
+    deck.shuffle!
   end
 
-  def deal_cards
-    player << deck.suffle!.shift
+  def deal_card
+    deck.shift
   end
 end
 
 class Card
-  def initialize
+  SUITS = %w(H D C S)
+  FACES = [*2..10] + %w(J Q K A)
+
+  def initialize(suit, face)
+    @suit = suit
+    @face = face
+  end
+
+  def suit
+    case @suit
+    when 'H'
+      'Hearts'
+    when 'D'
+      'Diamonds'
+    when 'C'
+      'Clubs'
+    when 'S'
+      'Spades'
+    end
+  end
+
+  def face
+    case @face
+    when 'J'
+      'Jack'
+    when 'Q'
+      'Queen'
+    when 'K'
+      'King'
+    when 'A'
+      'Ace'
+    else
+      @face
+    end
+  end
+
+  def to_s
+    "The #{face} of #{suit}."
   end
 end
 
 class Game
+  attr_accessor :player, :dealer, :deck
+
+  def initialize
+    @deck = Deck.new
+    @player = Player.new
+    @dealer = Dealer.new
+  end
+
+  def deal_cards
+    2.times do
+      player.collect_card(deck.deal_card)
+      dealer.collect_card(deck.deal_card)
+    end
+  end
+
+  def show_initial_cards
+    player.show_flop
+    dealer.show_flop
+  end
+
   def start
     deal_cards
     show_initial_cards
-    player_turn
-    dealer_turn
-    show_result
+    # player_turn
+    # dealer_turn
+    # show_result
   end
+
+
 end
 
 Game.new.start

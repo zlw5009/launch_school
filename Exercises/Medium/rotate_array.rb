d39679aa968715e4ee2e898d17268c1cebc66568
Zachary Williams <zlw5009@gmail.com>
2016-07-29 18:35:03 -0500

    Updating files

---
+++ Exercises/Medium/rotate_array.rb
@@ -0,0 +1,7 @@
+rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+rotate_array(['a']) == ['a']
+
+x = [1, 2, 3, 4]
+rotate_array(x) == [2, 3, 4, 1]   # => true
+x == [1, 2, 3, 4]   

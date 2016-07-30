83aee2769805e4adb6b752f24fac5cdf2c957922
Zachary Williams <zlw5009@gmail.com>
2016-07-29 19:03:51 -0500

    Rotation Part 2

--- Exercises/Medium/rotate_array.rb
+++ Exercises/Medium/rotate_array.rb
@@ -1,7 +1,20 @@
-rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
-rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
-rotate_array(['a']) == ['a']
+def rotate_array(array)
+  array[1..-1] + [array[0]] 
+end
 
-x = [1, 2, 3, 4]
-rotate_array(x) == [2, 3, 4, 1]   # => true
-x == [1, 2, 3, 4]   
+def rotate_rightmost_digits(number, x)
+  rotation = number.to_s.chars
+  rotation[-x..-1] = rotate_array(rotation[-x..-1])
+  rotation.join.to_i
+end
+
+p rotate_rightmost_digits(735291, 3) == 735912
+
+
+# rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+# rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+# rotate_array(['a']) == ['a']
+
+# x = [1, 2, 3, 4]
+# p rotate_array(x) == [2, 3, 4, 1]   # => true
+# x == [1, 2, 3, 4]   

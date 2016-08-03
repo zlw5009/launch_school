cb3a1f744fd8c8bfda19114c1bb60a51d0b42cf6
Zachary Williams <zlw5009@gmail.com>
2016-08-03 02:39:57 -0500

    Merge Sorted Lists Exercise

---
+++ Exercises/Advanced/merge_sorted_lists.rb
@@ -0,0 +1,34 @@
+# Write a method that takes two sorted Arrays as arguments, 
+# and returns a new Array that contains all elements from both arguments in sorted order.
+
+# You may not provide any solution that requires you to sort the result array. 
+# You must build the result array one element at a time in the proper order.
+
+# Your solution should not mutate the input arrays.
+
+# Examples:
+# merge([1, 5, 9], [2, 6, 8]) == [1, 2, 5, 6, 8, 9]
+# merge([1, 1, 3], [2, 2]) == [1, 1, 2, 2, 3]
+# merge([], [1, 4, 5]) == [1, 4, 5]
+# merge([1, 4, 5], []) == [1, 4, 5]
+
+# We need a comparison to see if one element is greater than or less than the other element
+# to determine the position of the element
+
+require 'pry'
+
+def merge(ary1, ary2)
+  merge = ary1 + ary2
+  result = []
+
+  merge.size.times do
+    result << merge.delete_at(merge.index(merge.min))
+  end
+  
+  result
+end
+
+p merge([1, 5, 9], [2, 6, 8]) == [1, 2, 5, 6, 8, 9]
+p merge([1, 1, 3], [2, 2]) == [1, 1, 2, 2, 3]
+p merge([], [1, 4, 5]) == [1, 4, 5]
+p merge([1, 4, 5], []) == [1, 4, 5]

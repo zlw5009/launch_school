bc3284fe36c524bf5952fdc8e2c938f77547624b
Zachary Williams <zlw5009@gmail.com>
2016-08-10 10:52:37 -0500

    Bubble sort and Quiver Themes

--- Exercises/Medium/bubble_sort.rb
+++ Exercises/Medium/bubble_sort.rb
@@ -18,25 +18,6 @@ def bubble_sort!(ary)
   nil
 end
 
-# def bubble_sort!(array)
-#   loop do
-#     swapped = false
-
-#     1.upto(array.size - 1) do |index|
-#       binding.pry
-#       next if array[index - 1] <= array[index]
-
-#       array[index - 1], array[index] = array[index], array[index - 1]
-
-#       swapped = true
-
-#     end
-
-#     break unless swapped
-#   end
-#   nil
-# end
-
 array = [6, 2, 7, 1, 4]
 p bubble_sort!(array)
 p array == [1, 2, 4, 6, 7]
